1233d05652d55f2dfc037405a0415b9381d90d44
honklin <121810694+honklin@users.noreply.github.com>
2024-10-07 13:40:32 -0400

    Update sensor.py

--- raspberry-pi/sensor.py
+++ raspberry-pi/sensor.py
@@ -1,1 +1,14 @@
+#type: ignore
+import time
+import board
+import digitalio 
+import analogio
 
+sensor = analogio.AnalogIn(board.A0)
+
+with open("/data.csv", "a") as datalog:
+    while True:
+        print(sensor.value)
+        datalog.write(f"{sensor.value}\n")
+        datalog.flush()
+        time.sleep(1)
