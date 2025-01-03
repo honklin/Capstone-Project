25c13d377de940f049ca420a5a381c2bdcfb9d13
honklin <121810694+honklin@users.noreply.github.com>
2025-01-03 13:34:26 -0500

    Update collection.py

--- code/collection.py
+++ code/collection.py
@@ -4,11 +4,15 @@ import board
 import digitalio 
 import analogio
 
-sensor = analogio.AnalogIn(board.A0)
+sensor1 = analogio.AnalogIn(board.A0)
+sensor2 = analogio.AnalogIn(board.A1)
+sensor3 = analogio.AnalogIn(board.A2)
 
-with open("/data.csv", "a") as datalog:
+with open("/data.csv", "w") as datalog:
     while True:
-        print(sensor.value)
-        datalog.write(f"{sensor.value}\n")
+        print(sensor1.value)
+        print(sensor2.value)
+        print(sensor3.value)
+        datalog.write(f"{sensor1.value, sensor2.value, sensor3.value}\n")
         datalog.flush()
-        time.sleep(1)
+        time.sleep(.5)
